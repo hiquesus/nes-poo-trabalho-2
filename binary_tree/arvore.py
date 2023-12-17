376ae8464677c90735279ffd5aaab3d6b013befe
Henrique <riquecbeltrao@hotmail.com>
2023-12-17 00:32:05 -0300

    work in progress

--- binary_tree/arvore.py
+++ binary_tree/arvore.py
@@ -1,4 +1,5 @@
 from typing import List, Union, Optional
+from collections import deque
 
 class ArvoreBinaria:
     def __init__(self, valor: float) -> None:
@@ -18,39 +19,73 @@ class ArvoreBinaria:
         """
         largest = self.valor
         
-        if self.esquerda is not None and self.esquerda.valor > largest:
+        if self.esquerda != None and self.esquerda.valor > largest:
             largest = self.esquerda.valor
         
-        if self.direita is not None and self.direita.valor > largest:
+        if self.direita != None and self.direita.valor > largest:
             largest = self.direita.valor
         
         if largest != self.valor:
             """efetua a troca e chama recursivamente o heapify até que não haja
             necessidade de troca (esteja em heap)"""
-            self.valor, largest = largest = self.valor
+            self.valor, largest = largest, self.valor
             self.heapify()
+
+    """def build_max_heap(self) -> None:
+        for i in range(len(self.to_array() // 2 - 1), -1, -1):
+            self.heapify(i)"""
     
     def to_array(self) -> List[float]:
+        """Transforma o objeto árvore binária em forma de lista
+
+        Returns:
+            List[float]: lista com os valores da árvore
+        """
         lista = []
-        if self is not None:
-            lista.append(self.valor)
-            if self.esquerda is not None:
-                lista.extend(self.esquerda.to_array())
-            if self.direita is not None:
-                lista.extend(self.direita.to_array())
+        fila = deque([self])
+        while fila:
+            no_atual = fila.popleft()
+            if no_atual:
+                lista.append(no_atual.valor)
+                fila.append(no_atual.esquerda)
+                fila.append(no_atual.direita)
+            else:
+                lista.append(None)
+        while lista and lista[-1] == None:
+            lista.pop()
         return lista
-    
-    def print_tree(self) -> None:
-        pass
-    
+        
+
+    def print_tree(self, space=0) -> None:
+        """Método recursivo que imprime toda a árvore
+
+        Args:
+            space (int, optional): Espaço para cada 'andar' da árvore.
+        """
+        if self:
+            space += 1
+            if self.direita:
+                self.direita.print_tree(space)
+            print(" " * 5 * space + str(self.valor))
+            if self.esquerda:
+                self.esquerda.print_tree(space)
+
     
 def construir_recursiva(i, arr):
     if i >= len(arr):
         return None
+    # inicia a árvore
     arvore = ArvoreBinaria(arr[i])
-    arvore.esquerda = construir_recursiva(2*i+1, arr)
-    arvore.direita = construir_recursiva(2*i+2, arr)
+    # caso exista, cria as subarvores esq e dir (caso existam na lista)
+    if arvore.valor:
+        if 2*i+1 <= len(arr):
+            arvore.esquerda = construir_recursiva(2*i+1, arr)
+        if 2*i+2 <= len(arr):
+            arvore.direita = construir_recursiva(2*i+2, arr)
+    # retorna a árvore criada
     return arvore
 
 def construir_arvore(arr):
+    # função que inicia a construção da árvore, chamando recursivamente
+    # construir_recursiva com i = 0.
     return construir_recursiva(0, arr)
